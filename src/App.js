5e342f87385cf018c00d565a204ade84be17f293
Artur Maslowski <bolo0010.pl@gmail.com>
2019-07-01 10:46:59 +0200

    Ver 1.0.0 release

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './css/App.css'
 
 class App extends React.Component {
   state = {
+    isEmpty: true,
     value: '',
     newValue: '',
     flag: false,
@@ -12,8 +13,11 @@ class App extends React.Component {
     score: '',
   }
 
+
   handleClick = (e) => {
-    if (this.state.score) {
+    const { flag, fun, score } = this.state;
+
+    if (score) {
       this.setState({
         score: ''
       })
@@ -21,19 +25,25 @@ class App extends React.Component {
     if (!isNaN(Number(e.target.textContent))) {
       const textContent = Number(e.target.textContent)
       if (this.state.value.length < 15) {
-        if (this.state.flag) {
+        if (flag) {
           let newValue = this.state.newValue
-          if (newValue[0] === '0' && newValue[1] !== '.') return
+          if (newValue[0] === '0' && newValue[1] !== '.') {
+            newValue = newValue.slice(0, 0);
+          }
           newValue += textContent
           this.setState({
-            newValue
+            newValue,
+            isEmpty: false
           })
         } else {
           let value = this.state.value
-          if (value[0] === '0' && value[1] !== '.') return
+          if (value[0] === '0' && value[1] !== '.') {
+            value = value.slice(0, 0);
+          }
           value += textContent
           this.setState({
-            value
+            value,
+            isEmpty: false
           })
         }
       }
@@ -44,81 +54,121 @@ class App extends React.Component {
           newValue: '',
           flag: false,
           fun: '',
-          score: ''
+          score: '',
+          isEmpty: true
         })
       } else if (e.target.textContent === '<') {
-        if (this.state.flag) {
+        if (flag) {
           let newValue = this.state.newValue
+          if (newValue.length < 2) {
+            this.setState({
+              isEmpty: true
+            })
+          }
+          if (newValue[0] === '-' && newValue.length < 3) {
+            this.setState({
+              isEmpty: true
+            })
+            newValue = newValue.slice(0, 1)
+          }
           newValue = newValue.slice(0, -1)
           this.setState({
             newValue
           })
         } else {
           let value = this.state.value
+          if (value.length < 2) {
+            this.setState({
+              isEmpty: true
+            })
+          }
+          if (value[0] === '-' && value.length < 3) {
+            this.setState({
+              isEmpty: true
+            })
+            value = value.slice(0, 1)
+          }
           value = value.slice(0, -1)
           this.setState({
             value
           })
         }
       } else if (e.target.textContent === '/' || e.target.textContent === '+' || e.target.textContent === '-' || e.target.textContent === 'X') {
+        if (!this.state.value || !this.state.newValue) {
+          this.setState({
+            isEmpty: true
+          })
+        }
         this.setState({
           flag: true,
-          fun: e.target.textContent
+          fun: e.target.textContent,
         })
       }
       else if (e.target.textContent === '=') {
-        let score = '';
+        if (score || !fun) {
+          this.setState({
+            isEmpty: true
+          })
+        }
+        let newScore = '';
         let value = this.state.value;
         let newValue = this.state.newValue;
         if (this.state.fun === '/') {
-          if (newValue === '0') {
+          if (newValue === '0' || newValue === '') {
             alert('Nie dziel przez 0!')
             return
           }
-          score = value / newValue;
-        } else if (this.state.fun === '+') {
-          score = Number(value) + Number(newValue)
-        } else if (this.state.fun === '-') {
-          score = value - newValue
-        } else if (this.state.fun === 'X') {
-          score = value * newValue
+          newScore = value / newValue;
+        } else if (fun === '+') {
+          newScore = Number(value) + Number(newValue)
+        } else if (fun === '-') {
+          newScore = value - newValue
+        } else if (fun === 'X') {
+          newScore = value * newValue
         }
         this.setState({
           value: '',
           newValue: '',
           flag: false,
           fun: '',
-          score
+          score: newScore
         })
       } else if (e.target.textContent === '.') {
-        if (this.state.flag) {
+        if (flag) {
           let newValue = this.state.newValue
+          if (!newValue) {
+            newValue += "0"
+          }
           if (newValue.includes('.')) return
           newValue += e.target.textContent
           this.setState({
-            newValue
+            newValue,
+            isEmpty: false
           })
         } else {
           let value = this.state.value
+          if (!value) {
+            value += "0"
+          }
           if (value.includes('.')) return
           value += e.target.textContent
           this.setState({
-            value
+            value,
+            isEmpty: false
           })
         }
       } else if (e.target.textContent === '+/-') {
-        if (this.state.flag) {
+        if (flag) {
           let newValue = this.state.newValue
           if (newValue === '0' || newValue === '') return
-          newValue = (-newValue)
+          newValue = String(-newValue)
           this.setState({
             newValue
           })
         } else {
-          // nie działa value i usuwanie się psuje
           let value = this.state.value
           if (value === '0' || value === '') return
-          value = (-value)
+          value = String(-value)
           this.setState({
             value
           })
@@ -130,7 +180,7 @@ class App extends React.Component {
   render() {
     return (
       <>
-        <Screen value={this.state.value} newValue={this.state.newValue} flag={this.state.flag} score={this.state.score} />
+        <Screen value={this.state.value} newValue={this.state.newValue} flag={this.state.flag} score={this.state.score} isEmpty={this.state.isEmpty} />
         <Keyboard click={this.handleClick} />
       </>
     );
--- src/Screen.js
+++ src/Screen.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 
-const Screen = ({ value, newValue, flag, score }) => {
+const Screen = ({ value, newValue, flag, score, isEmpty }) => {
     return (
-        <div className="screen"><span className="screen__numbers">{flag ? newValue : value}{score}</span></div>
+
+        <div className="screen"><span className="screen__numbers">{isEmpty ? "0" : null} {flag ? newValue : value} {score} </span></div>
     );
 }
 
