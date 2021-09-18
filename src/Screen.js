f59aba4231f4cddde506d73a0b6a1fd04ed069c7
Artur Maslowski <bolo0010.pl@gmail.com>
2021-09-18 14:12:00 +0200

    Fix styles

--- src/Screen.js
+++ src/Screen.js
@@ -3,7 +3,6 @@ import React from 'react';
 
 const Screen = ({value, newValue, flag, score, isEmpty}) => {
 	return (
-
 		<div className="screen"><span
 			className="screen__numbers">{isEmpty ? "0" : null} {flag ? newValue : value} {score} </span></div>
 	);
